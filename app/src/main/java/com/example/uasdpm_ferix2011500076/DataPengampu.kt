67f72a005b114a75c434f7c2dab6cb0f6a8feba3
ferix1234 <telokkodok87@gmail.com>
2022-06-20 16:58:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uasdpm_ferix2011500076/DataPengampu.kt
@@ -0,0 +1,11 @@
+package com.example.uasdpm_ferix2011500076
+
+class DataPengampu (
+    var nidn: String,
+    var nmDosen: String,
+    var jabatan: String,
+    var golonganpangkat: String,
+    var pendidikan: String,
+    var keahlian: String,
+    var programstudi: String,
+)
